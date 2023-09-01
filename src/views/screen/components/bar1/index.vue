d6506959227441cfcff5763573acb03392a090e4
BUNGA <3010086711@qq.com>
2023-09-01 17:55:02 +0800

    fix: 可视化没数据显示暂无数据

--- src/views/screen/components/bar1/index.vue
+++ src/views/screen/components/bar1/index.vue
@@ -3,81 +3,107 @@
     <dv-border-box10>
       <div class="h-full p-10">
         <div ref="chartRef" class="w-full h-full" />
+        <div class="w-full h-full">暂无数据</div>
       </div>
     </dv-border-box10>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, watch } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import * as echarts from 'echarts'
 import { BorderBox10 as DvBorderBox10 } from '@kjgl77/datav-vue3'
 import CustomTheme from '@/config/echarts/CustomTheme.json'
 import { reqStudentGrowthMonitor } from '@/api/screen'
 import type { StudentGrowthMonitorList } from '@/api/screen/type'
 
-const chartRef = ref()
-let chart: echarts.ECharts
-const dataArr = ref<StudentGrowthMonitorList>()
+type ECharts = echarts.ECharts
+type EChartOption = echarts.EChartOption
 
-watch(
-  dataArr,
-  (newVal) => {
-    if (newVal && newVal?.length) chart.setOption(update(newVal))
+const option: EChartOption = {
+  title: {
+    text: '学生成长监控',
   },
-  { deep: true },
-)
-
-const update = (data: StudentGrowthMonitorList): echarts.EChartOption => {
-  let xArr: string[] = []
-  let yArr: number[] = []
-  data.forEach((item) => {
-    xArr.push(item.growItemName)
-    yArr.push(item.personNum)
-  })
-  return {
-    title: {
-      text: '学生成长监控',
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow',
     },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'shadow',
+  },
+  legend: {
+    bottom: 0,
+  },
+  xAxis: {
+    type: 'value',
+    boundaryGap: [0, 0.01],
+  },
+  yAxis: {
+    type: 'category',
+    axisLabel: {
+      show: false,
+    },
+  },
+  series: [
+    {
+      name: '违规人次',
+      type: 'bar',
+      label: {
+        show: true,
+        formatter: '{b} {c}人',
       },
     },
-    legend: {
-      bottom: 0,
+  ],
+  graphic: {
+    type: 'text',
+    left: 'center',
+    top: 'middle',
+    silent: true,
+    style: {
+      fill: 'black',
+      fontWeight: 'bold',
+      text: '暂无数据',
+      fontSize: '26px',
+      fill: 'white',
     },
-    xAxis: {
-      type: 'value',
-      boundaryGap: [0, 0.01],
-    },
-    yAxis: {
-      type: 'category',
-      data: xArr,
-      axisLabel: {
-        show: false,
-      },
-    },
-    series: [
-      {
-        name: '违规人次',
-        type: 'bar',
-        data: yArr,
-        label: {
-          show: true,
-          formatter: '{b} {c}人',
-        },
-      },
-    ],
-  }
+  },
 }
 
-const fetch = async () => {
+const chartRef = ref()
+let chart: echarts.ECharts
+const chartData = ref<StudentGrowthMonitorList>()
+
+watch(chartData, (newVal) => {
+  if (newVal && newVal?.length) {
+    let x: string[] = []
+    let y: number[] = []
+    newVal.forEach((item) => {
+      x.push(item.growItemName)
+      y.push(item.personNum)
+    })
+    chart.setOption({
+      yAxis: {
+        data: x,
+      },
+      series: [
+        {
+          data: y,
+        },
+      ],
+    })
+  } else {
+    chart.setOption({
+      graphic: {
+        invisible: false, //是否可见，这里的意思是当没有数据时可见
+      },
+    })
+  }
+})
+
+const fetchData = async () => {
   try {
     chart.showLoading()
     const { data } = await reqStudentGrowthMonitor()
-    dataArr.value = data
+    chartData.value = data
   } catch (error) {
     console.log(error)
   } finally {
@@ -88,6 +114,7 @@ const fetch = async () => {
 onMounted(() => {
   echarts.registerTheme('CustomTheme', CustomTheme)
   chart = echarts.init(chartRef.value, 'CustomTheme')
-  fetch()
+  chart.setOption(option)
+  fetchData()
 })
 </script>
