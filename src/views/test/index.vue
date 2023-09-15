93f3d78b67c32bbef32c7c8d6acf7273e3c61d4a
BUNGA <3010086711@qq.com>
2023-09-15 14:16:06 +0800

    style: 定时脚本调用测试页面调整

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,31 +1,31 @@
 <template>
   <el-card class="m-20">
-    <el-button type="warning" @click="handleTask(1)">
-      计算每天积分（测试）
+    <el-button type="success" @click="handleTask(1)">
+      计算积分（每天）
     </el-button>
-    <el-button type="warning" @click="handleTask(2)">
-      计算每周积分（测试）
+    <el-button type="success" @click="handleTask(2)">
+      计算积分（每周）
     </el-button>
-    <el-button type="warning" @click="handleTask(3)">
-      计算每月积分（测试）
+    <el-button type="success" @click="handleTask(3)">
+      计算积分（每月）
     </el-button>
-    <el-button type="warning" @click="handleTask(4)">
-      计算每学期积分（测试）
+    <el-button type="success" @click="handleTask(4)">
+      计算期积分（每学）
     </el-button>
-    <el-button type="warning" @click="handleTask(5)">
-      计算每年积分（测试）
+    <el-button type="success" @click="handleTask(5)">
+      计算积分（每年）
     </el-button>
-    <el-button type="warning" @click="handleTask(6)">
+    <el-button type="success" @click="handleTask(6)">
       生成当前学年和学期
     </el-button>
-    <el-button type="warning" @click="handleTask(7)">
+    <el-button type="success" @click="handleTask(7)">
       统计本月榜单数据
     </el-button>
-    <el-button type="warning" @click="handleTask(8)">
-      统计学年榜单数据
+    <el-button type="success" @click="handleTask(8)">
+      成长计算每日定时补偿
     </el-button>
-    <el-button type="warning" @click="handleTask(9)">
-      统计学期榜单数据
+    <el-button type="success" @click="handleTask(9)">
+      自动采集未收集的水滴
     </el-button>
   </el-card>
 </template>
